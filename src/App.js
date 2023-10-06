ddc0383abaee7af34a0d1c5be50081ab0ef7fa05
thomas <tgiaimo@clemson.edu>
2023-10-06 14:06:12 -0400

    initial commit + site concept image

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+      <header class="App-spacer header">
+        <class class="header-column">welcome</class>
+        <class class="header-column">reels</class>
+        <class class="header-column">projects</class>
+        <class class="header-column">experience</class>
+        <class class="header-column">about</class>
+      </header>
+      <body class="App-body">
+        <text>test2</text>
+      </body>
+      <footer class="App-spacer">
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
